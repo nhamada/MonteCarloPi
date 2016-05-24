1deaf6c16ee2c74868cc35942df45486e19b31ee
Naohiro Hamada <nao.pinguinus@icloud.com>
2016-05-25 00:06:08 +0900

    Initial Commit

---
+++ MonteCarloPi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MonteCarloPi
+//
+//  Created by Naohiro Hamada on 2016/05/25.
+//  Copyright © 2016年 HaNoHito. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MonteCarloPi/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MonteCarloPi
+//
+//  Created by Naohiro Hamada on 2016/05/25.
+//  Copyright © 2016年 HaNoHito. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
